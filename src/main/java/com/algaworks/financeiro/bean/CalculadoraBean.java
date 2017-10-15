56020152b99596557fedb6058abae0b9c06aba91
JonesQuito <Jonesdhy@hotmail.com>
2017-10-14 21:26:53 -0300

    Ajustes na exibição do resultado.

---
+++ src/main/java/com/algaworks/financeiro/bean/CalculadoraBean.java
@@ -0,0 +1,52 @@
+package com.algaworks.financeiro.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CalculadoraBean {
+	private Double valorA;
+	private Double valorB;
+	private Double resultado;
+	private String nome;
+	
+	public void somar(){
+		this.resultado = this.valorA + this.valorB;
+	}
+	
+	public Double getValorA(){
+		return this.valorA;
+	}
+	
+	public void setValorA(Double valorA){
+		this.valorA = valorA;
+	}
+
+	public Double getValorB() {
+		return valorB;
+	}
+
+	public void setValorB(Double valorB) {
+		this.valorB = valorB;
+	}
+
+	public Double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+
+}
