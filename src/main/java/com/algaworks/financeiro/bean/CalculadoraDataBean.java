6e17a9cf961fafbe6bd4c11f28fe775b16dbf0ae
JonesQuito <Jonesdhy@hotmail.com>
2017-10-14 23:28:47 -0300

    Foi acrescentado o método que valida se o resultado é um dia util

--- src/main/java/com/algaworks/financeiro/bean/CalculadoraDataBean.java
+++ src/main/java/com/algaworks/financeiro/bean/CalculadoraDataBean.java
@@ -3,21 +3,40 @@ package com.algaworks.financeiro.bean;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class CalculadoraDataBean {
-	
+
 	private Date dataBase;
 	private Integer dias;
 	private Date resultado;
-	
-	public void adicionar(){
+
+	public void adicionar() {
 		Calendar dataCalculo = Calendar.getInstance();
 		dataCalculo.setTime(this.dataBase);
 		dataCalculo.add(Calendar.DAY_OF_MONTH, dias);
-		
 		this.resultado = dataCalculo.getTime();
+		validaResultado(resultado);
+	}
+	
+
+	public void validaResultado(Date resultado) {
+		Calendar calendario = Calendar.getInstance();
+		calendario.setTime(resultado);
+
+		int diaSemana = calendario.get(Calendar.DAY_OF_WEEK);
+		if (diaSemana < Calendar.MONDAY || diaSemana > Calendar.FRIDAY) {
+			setResultado(null);
+			dias = 0;
+			FacesMessage faces = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Dia inválida", "O resultado não é um dia útil");
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, faces);
+		}
+
 	}
 
 	public Date getDataBase() {
@@ -43,7 +62,5 @@ public class CalculadoraDataBean {
 	public void setResultado(Date resultado) {
 		this.resultado = resultado;
 	}
-	
-	
 
 }
